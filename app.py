27aa15d7f5ee4c8aceffc4347ffb7295259b49ee
Alan Zhu <alan.zhu39@gmail.com>
2024-08-05 19:57:20 -0700

    adding shortcode api route

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from instaloader import Instaloader, Post
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
   return "Hello, World!"
+
+@app.route("/api/<shortcode>")
+def api(shortcode):
+  L = Instaloader()
+  post = Post.from_shortcode(L.context, shortcode)
+  return post.video_url
